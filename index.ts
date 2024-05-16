ee19c3d30980181bafbe3aea328f4803e30f72e6
SaboorHaider <saboorzaidi5@gmail.com>
2024-05-16 12:29:13 +0500

    CLI Number Gussing Game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "please give a number between 1-6",
+    },
+]);
+
+if (answers.userGuessNumber === randomNumber) {
+    console.log(chalk.green("Congratulations! you win"));
+}else{
+    console.log(chalk.red("Unfortunately! you lose"));
+}
